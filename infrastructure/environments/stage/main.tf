b414fc9186193d6f2950854831eaa060d2b62c58
Mike Allison <mike@captainofbass.com>
2022-11-16 21:19:23 +0000

    fix: ecs deployment

--- infrastructure/environments/stage/main.tf
+++ infrastructure/environments/stage/main.tf
@@ -25,16 +25,48 @@ locals {
     vpc_id                  = module.stage.vpc.id
     is_public               = false
     subnets                 = module.stage.vpc.internal_subnets
-    alb_listener            = module.stage.internal_alb.aws_alb_listener
+    alb_listener_arn        = module.stage.internal_alb.aws_alb_listener["HTTP"].arn
     security_group_id       = module.stage.ecs.internal_ecs_security_group_id
     task_execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
     cluster                 = module.stage.ecs.ecs_cluster
+    alb_dns_name            = module.stage.internal_alb.dns_name
+    image_tag               = "stage"
+    zone_id                 = module.stage.route53_private_zone_id
+    alb_target_group        = local.alb_target_group
   }
 }
 
-module "api-gateway-stage" {
+module "api-gateway" {
   source        = "../../../apps/api-gateway/infrastructure"
   ecs_config    = local.public_stage_ecs_config
   env           = "stage"
   doppler_token = data.aws_ssm_parameter.doppler_api_gateway_stage_token.value
+}
+
+module "api-journeys" {
+  source        = "../../../apps/api-journeys/infrastructure"
+  ecs_config    = local.internal_stage_ecs_config
+  env           = "stage"
+  doppler_token = data.aws_ssm_parameter.doppler_api_journeys_stage_token.value
+}
+
+module "api-languages" {
+  source        = "../../../apps/api-languages/infrastructure"
+  ecs_config    = local.internal_stage_ecs_config
+  env           = "stage"
+  doppler_token = data.aws_ssm_parameter.doppler_api_languages_stage_token.value
+}
+
+module "api-users" {
+  source        = "../../../apps/api-users/infrastructure"
+  ecs_config    = local.internal_stage_ecs_config
+  env           = "stage"
+  doppler_token = data.aws_ssm_parameter.doppler_api_users_stage_token.value
+}
+
+module "api-videos" {
+  source        = "../../../apps/api-videos/infrastructure"
+  ecs_config    = local.internal_stage_ecs_config
+  env           = "stage"
+  doppler_token = data.aws_ssm_parameter.doppler_api_videos_stage_token.value
 }
