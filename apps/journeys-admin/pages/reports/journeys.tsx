627d8dbad53d4fc8185fd14b0dc107738d390d11
jernst7 <JEErnst15@gmail.com>
2023-01-16 21:13:09 +0000

    feat: added basic table and query

--- apps/journeys-admin/pages/reports/journeys.tsx
+++ apps/journeys-admin/pages/reports/journeys.tsx
@@ -7,19 +7,44 @@ import {
 } from 'next-firebase-auth'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
+import { gql, useQuery } from '@apollo/client'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
 import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
+import TableContainer from '@mui/material/TableContainer'
+import Table from '@mui/material/Table'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableRow from '@mui/material/TableRow'
+import TableCell from '@mui/material/TableCell'
 import Box from '@mui/material/Box'
 import { PageWrapper } from '../../src/components/PageWrapper'
 import i18nConfig from '../../next-i18next.config'
+import { VisitorsConnection } from '../../../api-journeys/src/app/__generated__/graphql'
 import { MemoizedDynamicReport } from '../../src/components/DynamicPowerBiReport'
 import { JourneysReportType } from '../../__generated__/globalTypes'
+
+export const GET_VISITOR_CONNECTION = gql`
+  query VisitorsConnection($teamId: String!) {
+    visitorsConnection(teamId: $teamId) {
+      edges {
+        cursor
+        node {
+          id
+          createdAt
+        }
+      }
+    }
+  }
+`
 
 function ReportsJourneysPage(): ReactElement {
   const router = useRouter()
   const { t } = useTranslation('apps-journeys-admin')
   const AuthUser = useAuthUser()
+  const { data } = useQuery<VisitorsConnection>(GET_VISITOR_CONNECTION, {
+    variables: { teamId: 'jfp-team' }
+  })
 
   return (
     <>
@@ -30,7 +55,32 @@ function ReportsJourneysPage(): ReactElement {
         router={router}
       >
         <Box sx={{ height: 'calc(100vh - 48px)' }}>
-          <MemoizedDynamicReport reportType={JourneysReportType.multipleFull} />
+          {/* <MemoizedDynamicReport reportType={JourneysReportType.multipleFull} /> */}
+          <TableContainer>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>S.No</TableCell>
+                  <TableCell align="right">Item</TableCell>
+                  <TableCell align="right">Quantity&nbsp;(kg)</TableCell>
+                  <TableCell align="right">Price&nbsp;($)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {console.log(data)}
+                {/* {data != null ? data.edges.map((row) => (
+                  <TableRow key={row.number}>
+                    <TableCell component="th" scope="row">
+                      {row.number}
+                    </TableCell>
+                    <TableCell align="right">{row.item}</TableCell>
+                    <TableCell align="right">{row.qty}</TableCell>
+                    <TableCell align="right">{row.price}</TableCell>
+                  </TableRow> 
+                )): <></>} */}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </PageWrapper>
     </>
