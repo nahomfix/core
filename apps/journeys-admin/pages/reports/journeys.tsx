9a649eb5fb12e3e43f169f832cb6d7a2244caa63
JesseErnst <jesse@onsiteanalytics.co>
2023-01-26 21:38:26 +0000

    feat: save before main merge

--- apps/journeys-admin/pages/reports/journeys.tsx
+++ apps/journeys-admin/pages/reports/journeys.tsx
@@ -7,39 +7,19 @@ import {
 } from 'next-firebase-auth'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
-import { gql, useQuery } from '@apollo/client'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
 import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
 import Box from '@mui/material/Box'
 import { PageWrapper } from '../../src/components/PageWrapper'
-import { VisitorList } from '../../src/components/VisitorList'
 import i18nConfig from '../../next-i18next.config'
-import { VisitorsConnection } from '../../../api-journeys/src/app/__generated__/graphql'
 import { MemoizedDynamicReport } from '../../src/components/DynamicPowerBiReport'
 import { JourneysReportType } from '../../__generated__/globalTypes'
-
-export const GET_VISITOR_CONNECTION = gql`
-  query VisitorsConnection($teamId: String!) {
-    visitorsConnection(teamId: $teamId) {
-      edges {
-        cursor
-        node {
-          id
-          createdAt
-        }
-      }
-    }
-  }
-`
 
 function ReportsJourneysPage(): ReactElement {
   const router = useRouter()
   const { t } = useTranslation('apps-journeys-admin')
   const AuthUser = useAuthUser()
-  const { data } = useQuery<VisitorsConnection>(GET_VISITOR_CONNECTION, {
-    variables: { teamId: 'jfp-team' }
-  })
 
   return (
     <>
@@ -50,8 +30,7 @@ function ReportsJourneysPage(): ReactElement {
         router={router}
       >
         <Box sx={{ height: 'calc(100vh - 48px)' }}>
-          {/* <MemoizedDynamicReport reportType={JourneysReportType.multipleFull} /> */}
-          {data != null ? <VisitorList {...{ edges: data.edges }} /> : <></>}
+          <MemoizedDynamicReport reportType={JourneysReportType.multipleFull} />
         </Box>
       </PageWrapper>
     </>
--- apps/journeys-admin/src/components/VisitorList/VisitorList.tsx
+++ apps/journeys-admin/src/components/VisitorList/VisitorList.tsx
@@ -5,13 +5,43 @@ import TableHead from '@mui/material/TableHead'
 import TableBody from '@mui/material/TableBody'
 import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
+import Button from '@mui/material/Button'
 import { VisitorEdge } from '../../api-journeys/src/app/__generated__/graphql'
+
+// README: This is the implementation for the visitorList. Imports, DB call, and implementation included
+
+// import { VisitorsConnection } from '../../../api-journeys/src/app/__generated__/graphql'
+// import { gql, useQuery } from '@apollo/client'
+// import { VisitorList } from '../../src/components/VisitorList'
+
+// const { data } = useQuery<VisitorsConnection>(GET_VISITOR_CONNECTION, {
+//   variables: { teamId: 'jfp-team' }
+// })
+
+// export const GET_VISITOR_CONNECTION = gql`
+//   query VisitorsConnection($teamId: String!) {
+//     visitorsConnection(teamId: $teamId) {
+//       edges {
+//         cursor
+//         node {
+//           id
+//           createdAt
+//         }
+//       }
+//     }
+//   }
+// `
+
+// {data != null ? <VisitorList {...{ edges: data.edges }} /> : <></>}
+
+
 
 interface Props {
   edges: VisitorEdge[]
+  onClick?: () => void
 }
 
-export function VisitorList({ edges }: Props): ReactElement {
+export function VisitorList({ edges, onClick }: Props): ReactElement {
   return (
     <TableContainer>
       <Table aria-label="simple table">
@@ -29,7 +59,8 @@ export function VisitorList({ edges }: Props): ReactElement {
               </TableCell>
               <TableCell align="left">{row.node.createdAt}</TableCell>
             </TableRow>
-          ))}
+          ))} <Button onCLick={onClick}>
+          <TableRow/>Load More</Button>
         </TableBody>
       </Table>
     </TableContainer>
