a0c13c38c0be859113bfc54694e83b71a5c3218c
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-09-06 02:30:08 +0000

    fix: restore apollo client

--- apps/journeys-admin/src/libs/apolloClient/apolloClient.ts
+++ apps/journeys-admin/src/libs/apolloClient/apolloClient.ts
@@ -4,13 +4,26 @@ import {
   createHttpLink
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-import { User as AuthUser } from 'next-firebase-auth'
+import jwt from 'jsonwebtoken'
+import { AuthUser } from 'next-firebase-auth'
 import { useMemo } from 'react'
 
 import { cache } from './cache'
 
+export function isTokenExpired(token: string): boolean {
+  try {
+    const decodedToken = jwt.decode(token) as { exp: number }
+    const tokenExpiresAt = decodedToken.exp
+    const now = Math.floor(Date.now() / 1000)
+    return now > tokenExpiresAt
+  } catch (error) {
+    console.error(error)
+    return true
+  }
+}
+
 export function createApolloClient(
-  user?: AuthUser
+  token: string
 ): ApolloClient<NormalizedCacheObject> {
   const isSsrMode = typeof window === 'undefined'
   const httpLink = createHttpLink({
@@ -18,12 +31,28 @@ export function createApolloClient(
   })
 
   const authLink = setContext(async (_, { headers }) => {
-    const token = await user?.firebaseUser?.getIdToken()
+    // If this is SSR, DO NOT PASS THE REQUEST HEADERS.
+    // Just send along the authorization headers.
+    // The **correct** headers will be supplied by the `getServerSideProps` invocation of the query
+
+    let refreshToken
+    // Check if we are in SSR mode and load the AuthUser module
+    if (isSsrMode) {
+      const { verifyIdToken } = await import(
+        /* webpackChunkName: "next-firebase-auth" */
+        'next-firebase-auth'
+      )
+      const authUser: AuthUser = await verifyIdToken(token)
+      const newToken = await authUser.getIdToken(true)
+      if (newToken != null) {
+        refreshToken = newToken
+      }
+    }
 
     return {
       headers: {
         ...(!isSsrMode ? headers : []),
-        Authorization: token
+        Authorization: isTokenExpired(token) ? refreshToken : token
       }
     }
   })
@@ -37,8 +66,6 @@ export function createApolloClient(
   })
 }
 
-export function useApollo(
-  user?: AuthUser
-): ApolloClient<NormalizedCacheObject> {
-  return useMemo(() => createApolloClient(user), [user])
+export function useApollo(token: string): ApolloClient<NormalizedCacheObject> {
+  return useMemo(() => createApolloClient(token), [token])
 }
